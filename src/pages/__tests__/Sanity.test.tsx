55e942acfcb64a543500b92abbfca3d234170edb
if3ll <sunnwydays@gmail.com>
2025-01-03 09:52:30 -0500

    Remove sanity test

--- src/pages/__tests__/Sanity.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Courses from '../Courses';
-
-describe('CourseCard', () => {
-    it('renders the courses component', () => {
-        render(<Courses />);
-        expect(screen.getByText('Courses')).toBeInTheDocument();
-    });
-});
