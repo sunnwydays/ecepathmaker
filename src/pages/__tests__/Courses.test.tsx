6209cb94bff38fea433a63afa7c0cbd180bf8d1b
if3ll <sunnwydays@gmail.com>
2025-01-03 17:36:11 -0500

    Write courses page test

--- src/pages/__tests__/Courses.test.tsx
+++ src/pages/__tests__/Courses.test.tsx
@@ -1,16 +1,46 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Courses from "../Courses";
 
 describe('Courses page', () => {
-    it('filters by category correctly', () => {
+    beforeEach(() => {
         render(<Courses/>);
-        const inputBox = screen.getAllByPlaceholderText('')
-    })
-});
+    });
 
-describe('Courses page', () => {
-    it('searches by detail correctly', () => {
-        render(<Courses/>);
-        const inputBox = screen.getAllByPlaceholderText('')
-    })
+    it('filters by category correctly', () => {
+        // stream 5
+        const stream5Select = screen.getByTestId('stream-5');
+        fireEvent.click(stream5Select);
+
+        // available in the fall
+        const fallSelect = screen.getByTestId('fall-select');
+        fireEvent.click(fallSelect);
+
+        expect(screen.getByText(/ECE302/)).toBeInTheDocument();
+        expect(screen.queryByText(/APS360/)).not.toBeInTheDocument();
+    });
+
+    it('searches by course name correctly', () => {
+        const inputBox = screen.getByPlaceholderText('Intro to Electronics');
+        fireEvent.change(inputBox, { target: { value: 'electronics' } });
+
+        expect(screen.getByText("ECE331: Analog Electronics")).toBeInTheDocument();
+        expect(screen.getByText("ECE334: Digital Electronics")).toBeInTheDocument();
+        expect(screen.queryByText(/ECE302/)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Random Processes/)).not.toBeInTheDocument();
+    });
+
+    it('shows no results message for garbage search', () => {
+        const searchInput = screen.getByPlaceholderText('Intro to Electronics');
+        fireEvent.change(searchInput, { target: { value: 'xxxxx' } });
+        
+        expect(screen.getByText(/No matching courses in database/i)).toBeInTheDocument();
+    });
+
+    it('resets filters correctly', () => {
+        const resetButton = screen.getByTestId('reset-filters');
+        fireEvent.click(resetButton);
+        
+        expect(screen.getByText(/APS360/)).toBeInTheDocument();
+        expect(screen.getByText(/ECE302/)).toBeInTheDocument();
+    });
 });
