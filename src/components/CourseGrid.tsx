fb0e63798f5c563a445f20ec097bd1050ee4b754
sunny <sunnwydays@gmail.com>
2025-01-12 00:11:32 -0500

    Prevent F only course from being placed in S slot and vice versa

--- src/components/CourseGrid.tsx
+++ src/components/CourseGrid.tsx
@@ -86,6 +86,16 @@ const CourseGrid:FC<{courses: CourseList;}> = ({ courses }) => {
         // also looking online for documentation, videos, and examples
         const sourceContainer = coursesUsed[active.id];
         if (over) {
+            // term offering restrictions
+            const targetTerm = (over.id as string).includes('F') ? 'F' : 'S';
+            const course = courses[active.id as string];
+            
+            if ((course.onlyF && targetTerm === 'S') || 
+                (course.onlyS && targetTerm === 'F')) {
+                return;
+            }
+
+            // same slot
             if (sourceContainer === over.id) return;
             
             setCoursesOnGrid(prev => ({
