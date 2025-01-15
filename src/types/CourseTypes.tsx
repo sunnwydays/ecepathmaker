88eefd8a20512f40142d95c501daf8a3c50ac98a
sunny <sunnwydays@gmail.com>
2025-01-15 11:39:22 -0500

    Implement GridPosition type

--- src/types/CourseTypes.tsx
+++ src/types/CourseTypes.tsx
@@ -52,18 +52,20 @@ export interface StreamRequirements {
     hasHSS: boolean;
 };
 
+type GridPosition = 
+    | '3F.1' | '3F.2' | '3F.3' | '3F.4' | '3F.5'
+    | '3S.1' | '3S.2' | '3S.3' | '3S.4' | '3S.5'
+    | '4F.1' | '4F.2' | '4F.3' | '4F.4' | '4F.5'
+    | '4S.1' | '4S.2' | '4S.3' | '4S.4' | '4S.5'
+    | 'XX.1' | 'XX.2' | 'XX.3' | 'XX.4' | 'XX.5';
+
+export type CoursesOnGrid = Record<GridPosition, string>;
+export type CoursesUsed = Record<string, string>;
+
 export interface CourseGridProps {
     courses: CourseList;
-    coursesUsed: {
-        [key: string]: string;
-    };
-    setCoursesUsed?: React.Dispatch<React.SetStateAction<{
-        [key: string]: string;
-    }>>;
-    coursesOnGrid: {
-        [key: string]: string;
-    };
-    setCoursesOnGrid?: React.Dispatch<React.SetStateAction<{
-        [key: string]: string;
-    }>>;
+    coursesUsed: CoursesUsed;
+    setCoursesUsed?: React.Dispatch<React.SetStateAction<CoursesUsed>>;
+    coursesOnGrid: CoursesOnGrid;
+    setCoursesOnGrid?: React.Dispatch<React.SetStateAction<CoursesOnGrid>>;
 }
