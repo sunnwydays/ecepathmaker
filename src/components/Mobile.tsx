bde9e7ad35e42b80b2e48c705ba81b1b8707bb0b
sunny <sunnwydays@gmail.com>
2025-01-19 10:28:31 -0500

    Change mobile message to say small screens instead of mobile devices

--- src/components/Mobile.tsx
+++ src/components/Mobile.tsx
@@ -2,7 +2,7 @@ const Mobile = () => {
     return (
         <div className="sm:hidden block fixed bottom-0 left-0 right-0 bg-gray-800 text-white p-4 z-50">
             <p className="text-center">
-                This site is not optimized for mobile. Please view on a desktop for the best experience.
+                This site is not optimized for small screens. Please view on a desktop for the best experience.
             </p>
         </div>
     );
