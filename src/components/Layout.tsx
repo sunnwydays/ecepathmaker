f4df0278dfe333a566bdf138def74f87e26b0874
sunny <sunnwydays@gmail.com>
2025-01-18 23:50:30 -0500

    Add a message about not using mobile on smaller screens

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,17 +2,18 @@ import { FC, ReactNode } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { Analytics } from "@vercel/analytics/react"
+import Mobile from './Mobile';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
-  
   return (
     <div className="min-h-screen flex flex-col">
       <div className="h-24 md:block hidden" />
       <Navbar />
+      <Mobile />
       <main className="flex-grow mx-auto xl:wp lg:w-wpl w-wps my-12">
         {children}
         <Analytics />
---
+++ src/components/Mobile.tsx
@@ -0,0 +1,11 @@
+const Mobile = () => {
+    return (
+        <div className="sm:hidden block fixed bottom-0 left-0 right-0 bg-gray-800 text-white p-4 z-50">
+            <p className="text-center">
+                This site is not optimized for mobile. Please view on a desktop for the best experience.
+            </p>
+        </div>
+    );
+}
+
+export default Mobile;
