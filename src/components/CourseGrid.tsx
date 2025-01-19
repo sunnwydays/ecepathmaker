73fac015da614bb0b7ca4fdf403ff5569829192e
sunny <sunnwydays@gmail.com>
2025-01-18 23:06:33 -0500

    Return course to bucket if dragging along the same term as before with missing prereq

--- src/components/CourseGrid.tsx
+++ src/components/CourseGrid.tsx
@@ -199,7 +199,7 @@ const CourseGrid:FC<CourseGridProps> = ({ courses, coursesOnGrid, coursesUsed, s
 
             if (!validYearTerms[yearTerm]) {
                 // somewhat accounting for moving prereq after moving the course
-                if (sourceContainer === over.id) {
+                if (getYearTerm(sourceContainer as GridPosition) === getYearTerm(over.id as GridPosition)) {
                     // return course to the bucket
                     setCoursesOnGrid(prev => ({
                         ...prev, 
