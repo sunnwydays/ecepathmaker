bd2060cbd02b3793b499e967fd017714a218a661
sunny <sunnwydays@gmail.com>
2025-01-19 11:06:12 -0500

    Delete mobile message

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,6 @@ import { FC, ReactNode } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { Analytics } from "@vercel/analytics/react"
-import Mobile from './Mobile';
 
 interface LayoutProps {
   children: ReactNode;
@@ -13,7 +12,6 @@ const Layout: FC<LayoutProps> = ({ children }) => {
     <div className="min-h-screen flex flex-col">
       <div className="h-24 md:block hidden" />
       <Navbar />
-      <Mobile />
       <main className="flex-grow md:mx-auto mx-3 xl:wp lg:w-wpl w-wps my-12">
         {children}
         <Analytics />
