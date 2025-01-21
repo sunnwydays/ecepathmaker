137f2e50daf81658431516023761434bcb5a9b8f
sunny <sunnwydays@gmail.com>
2025-01-21 14:55:55 -0500

    Use Vercel SpeedInsights

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 interface LayoutProps {
   children: ReactNode;
@@ -15,6 +16,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
       <main className="flex-grow md:mx-auto mx-3 xl:wp lg:w-wpl w-wps my-12">
         {children}
         <Analytics />
+        <SpeedInsights />
       </main>
       <Footer />
     </div>
