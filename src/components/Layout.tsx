37e2662a998bfdb3f6203128c1e03cedd6c779cd
sunnwydays <sunny.wu@autodrive.utoronto.ca>
2025-01-21 15:01:22 -0500

    Revert adding Vercel analytics because it doesn't deploy

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,6 @@ import { FC, ReactNode } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 interface LayoutProps {
   children: ReactNode;
@@ -16,7 +15,6 @@ const Layout: FC<LayoutProps> = ({ children }) => {
       <main className="flex-grow md:mx-auto mx-3 xl:wp lg:w-wpl w-wps my-12">
         {children}
         <Analytics />
-        <SpeedInsights />
       </main>
       <Footer />
     </div>
