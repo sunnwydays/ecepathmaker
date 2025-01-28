c800f0fc8a5adacd7647cce11e564b6297a74737
sunny <sunnwydays@gmail.com>
2025-01-28 18:12:01 -0500

    Refactor Droppable for readability

--- src/components/Droppable.tsx
+++ src/components/Droppable.tsx
@@ -1,14 +1,14 @@
-import {useDroppable} from '@dnd-kit/core';
-import { FC, ReactNode } from 'react';
+import { useDroppable } from "@dnd-kit/core";
+import { FC, ReactNode } from "react";
 
 interface DroppableProps {
-    children: ReactNode;
-    id: string;
-    valid: boolean;
-};
+  children: ReactNode;
+  id: string;
+  valid: boolean;
+}
 
-const Droppable:FC<DroppableProps> = (props) => {
-  const {isOver, setNodeRef} = useDroppable({
+const Droppable: FC<DroppableProps> = (props) => {
+  const { isOver, setNodeRef } = useDroppable({
     id: props.id,
   });
   const styleClasses = `
@@ -17,20 +17,23 @@ const Droppable:FC<DroppableProps> = (props) => {
         flex items-center justify-center
         text-neutral3
         rounded-md
-        ${props.valid ? 
-          isOver ? 'bg-green1 bg-opacity-70 size-36' : 'bg-neutral1 size-32'
-          : 'bg-comp3 bg-opacity-55 size-32 text-opacity-0'
+        ${
+          props.valid
+            ? isOver
+              ? "bg-green1 bg-opacity-70 size-36"
+              : "bg-neutral1 size-32"
+            : "bg-comp3 bg-opacity-55 size-32 text-opacity-0"
         }
     `;
-  
+
   return (
     <div className="relative size-32 flex items-center justify-center">
-        <div ref={setNodeRef} className={styleClasses}>
-          {props.children}
-        </div>
+      <div ref={setNodeRef} className={styleClasses}>
+        {props.children}
+      </div>
     </div>
   );
-}
-// these bunch of divs allow the draggable to not be blocked by the droppable when dragging
+};
+// Nested divs prevent the droppables from blocking the draggable when dragging
 
 export default Droppable;
