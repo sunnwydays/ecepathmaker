9791d73f95bf5a531f01f261964aeedcf4d6364c
sunny <sunnwydays@gmail.com>
2025-02-20 10:08:00 -0500

    Move grid components into nested folder

---
+++ src/components/grid/CourseGrid.tsx
@@ -0,0 +1,556 @@
+import Droppable from "./Droppable";
+
+import { FC, useState, useEffect, useMemo, useRef, createRef } from "react";
+import MakerCard from "./MakerCard";
+import {
+  CourseGridProps,
+  FilterState,
+  ValidYearTerms,
+  StreamRequirements,
+  GridPosition,
+  CoursesUsed,
+} from "../../types/CourseTypes";
+// @ts-expect-error - use-react-screenshot is not typed
+import { useScreenshot, createFileName } from "use-react-screenshot";
+
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  MouseSensor,
+  TouchSensor,
+  UniqueIdentifier,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { createPortal } from "react-dom";
+import Filter from "../Filter";
+import WillYouGraduate from "../WillYouGraduate";
+import { getValidYearTerms } from "../../utils/getValidYearTerms";
+import { getYearTerm } from "../../utils/getYearTerm";
+import Announcement from "../Announcement";
+
+enum DropError {
+  NONE = "NONE",
+  PREREQ = "PREREQ",
+  TERM = "TERM",
+}
+
+const CourseGrid: FC<CourseGridProps> = ({
+  courses,
+  coursesOnGrid,
+  coursesUsed,
+  setCoursesOnGrid,
+  setCoursesUsed,
+  setCustomInfo,
+  setPreqString,
+}) => {
+  const [filters, setFilters] = useState<FilterState>({
+    searchTerm: "",
+    streams: [],
+    availableF: true,
+    availableS: true,
+    isCS: false,
+    isHSS: false,
+    isSciMath: false,
+    isArtSci: false,
+    isEng: false,
+  });
+
+  const filterCourses = (courseCode: string): boolean => {
+    const course = courses[courseCode];
+    if (!course) return false;
+
+    if (
+      filters.searchTerm &&
+      !courseCode.toLowerCase().includes(filters.searchTerm.toLowerCase()) &&
+      !course.name.toLowerCase().includes(filters.searchTerm.toLowerCase())
+    ) {
+      return false;
+    }
+
+    if (
+      filters.streams.length > 0 &&
+      !course.streams?.some((stream) => filters.streams.includes(stream))
+    ) {
+      return false;
+    }
+
+    if (!filters.availableF && course.onlyF) return false;
+    if (!filters.availableS && course.onlyS) return false;
+
+    if (filters.isCS && !course.isCS) return false;
+    if (filters.isHSS && !course.isHSS) return false;
+    if (filters.isSciMath && !course.isSciMath) return false;
+    if (filters.isArtSci && !course.isArtSci) return false;
+    if (filters.isEng && course.isArtSci) return false;
+
+    return true;
+  };
+
+  const initialValidYearTerms = useMemo<ValidYearTerms>(
+    () =>
+      ({
+        "3F": true,
+        "3S": true,
+        "4F": true,
+        "4S": true,
+        XX: true,
+      } as ValidYearTerms),
+    []
+  );
+
+  const [validYearTerms, setValidYearTerms] = useState<ValidYearTerms>(
+    initialValidYearTerms
+  );
+  const [activeCourse, setActiveCourse] = useState<UniqueIdentifier | null>(
+    null
+  );
+
+  const conditions = useMemo(() => {
+    const gridCourses = Object.values(coursesOnGrid).filter(
+      (code) => code !== ""
+    );
+
+    // Count courses per stream
+    const streamInfo = gridCourses.reduce((acc, code) => {
+      const courseStreams = courses[code]?.streams || [];
+      const isKernel = courses[code]?.kernel || false;
+
+      courseStreams.forEach((stream) => {
+        if (!acc[stream]) {
+          acc[stream] = { count: 0, hasKernel: false };
+        }
+        acc[stream].count++;
+        if (isKernel) acc[stream].hasKernel = true;
+      });
+      return acc;
+    }, {} as Record<number, { count: number; hasKernel: boolean }>);
+
+    const streamCounts = Array.from({ length: 6 }, (_, i) => i + 1).reduce(
+      (acc, stream) => {
+        acc[stream] = streamInfo[stream]?.count || 0;
+        return acc;
+      },
+      {} as Record<number, number>
+    );
+
+    // Get streams meeting requirements
+    const depthStreams = Object.entries(streamInfo)
+      .filter(([, info]) => info.count >= 3 && info.hasKernel)
+      .map(([stream]) => Number(stream));
+
+    const breadthStreams = [
+      ...Object.entries(streamInfo)
+        .filter(
+          ([stream, info]) =>
+            info.count >= 1 &&
+            info.hasKernel &&
+            !depthStreams.includes(Number(stream))
+        )
+        .map(([stream]) => Number(stream)),
+    ];
+
+    const numDepth = depthStreams.length;
+    const hasBreadth =
+      breadthStreams.length >= 2 ||
+      (breadthStreams.length === 1 && numDepth >= 3) ||
+      numDepth >= 4;
+    const hasDepth = depthStreams.length >= 2;
+
+    let ceOrEE = null; // null, 'CE', 'EE', or 'ECE'
+    if (hasDepth && hasBreadth) {
+      const numCEDepth = depthStreams.filter(
+        (stream) => stream === 5 || stream === 6
+      ).length;
+      const numEEDepth = depthStreams.length - numCEDepth;
+      const numCEBreadth = breadthStreams.filter(
+        (stream) => stream === 5 || stream === 6
+      ).length;
+      const numEEBreadth = breadthStreams.length - numCEBreadth;
+
+      // Logic can't be based only on count due to possibility of many EE
+      // depths and need to differentiate depth and breadth
+      const countCE = numCEDepth * 3 + numCEBreadth;
+
+      if (countCE >= 4) {
+        switch (numEEDepth) {
+          case 0:
+            ceOrEE = "CE";
+            break;
+          case 1:
+            // 0 EE breadth not possible
+            if (numEEBreadth === 1) ceOrEE = "CE";
+            else ceOrEE = "ECE";
+            break;
+          default:
+            ceOrEE = "ECE";
+            break;
+        }
+      } else {
+        ceOrEE = "EE";
+      }
+    }
+
+    const isCapstone = (course1: string, course2: string): boolean => {
+      return (
+        gridCourses.includes(course1) &&
+        gridCourses.includes(course2) &&
+        coursesUsed[course1][0] === "4" &&
+        coursesUsed[course2][0] === "4" &&
+        coursesUsed[course1][1] !== coursesUsed[course2][1]
+      );
+    };
+
+    const hasCapstone = [
+      ["ECE496", "ECE497"],
+      ["APS490", "APS491"],
+      ["BME498", "BME499"],
+    ].some(([course1, course2]) => isCapstone(course1, course2));
+
+    return {
+      hasCS: gridCourses.filter((code) => courses[code]?.isCS).length >= 4,
+      hasHSS: gridCourses.filter((code) => courses[code]?.isHSS).length >= 2,
+      streamCounts,
+      breadthStreams,
+      depthStreams,
+      hasBreadth,
+      hasDepth,
+      hasEconomics: gridCourses.includes("ECE472"),
+      hasCapstone,
+      hasSciMath: gridCourses.some((code) => courses[code]?.isSciMath),
+      ceOrEE,
+    } as StreamRequirements;
+  }, [coursesOnGrid, courses, coursesUsed]);
+
+  const sensors = useSensors(
+    useSensor(MouseSensor, {
+      activationConstraint: {
+        distance: 2,
+      },
+    }),
+    useSensor(TouchSensor, {
+      activationConstraint: {
+        delay: 100,
+        tolerance: 5,
+      },
+    })
+  );
+
+  const handleDragStart = (e: DragStartEvent) => {
+    setActiveCourse(e.active.id);
+    const ValidYearTermsProps = {
+      coursesOnGrid,
+      coursesUsed,
+      course: courses[e.active.id],
+    };
+    const validYearTerms = getValidYearTerms(ValidYearTermsProps);
+    setValidYearTerms(validYearTerms);
+  };
+
+  const handleDragEnd = (e: DragEndEvent) => {
+    setActiveCourse(null);
+    setValidYearTerms(initialValidYearTerms);
+    const { over, active } = e;
+
+    // jest testing good for setting objectives
+    // console logging very useful for debugging here along with dev tools
+    // also looking online for documentation, videos, and examples
+    const sourceContainer = coursesUsed[active.id];
+    if (over) {
+      // term offering restrictions
+      const targetTerm = (over.id as string)[1];
+      const course = courses[active.id as string];
+
+      if (
+        (course.onlyF && targetTerm === "S") ||
+        (course.onlyS && targetTerm === "F")
+      ) {
+        if (!validYearTerms[getYearTerm(sourceContainer)]) {
+          // return course to the bucket
+          setCoursesOnGrid((prev) => ({
+            ...prev,
+            ...(sourceContainer && { [sourceContainer]: "" }),
+          }));
+          setCoursesUsed({ ...coursesUsed, [active.id]: "" as GridPosition });
+        }
+        setDropError(DropError.TERM);
+        return;
+      }
+
+      const yearTerm = getYearTerm(over.id as GridPosition);
+
+      if (!validYearTerms[yearTerm]) {
+        // somewhat accounting for moving prereq after moving the course
+        if (!validYearTerms[getYearTerm(sourceContainer)]) {
+          // return course to the bucket
+          setCoursesOnGrid((prev) => ({
+            ...prev,
+            ...(sourceContainer && { [sourceContainer]: "" }),
+          }));
+          setCoursesUsed({ ...coursesUsed, [active.id]: "" as GridPosition });
+        }
+        setDropError(DropError.PREREQ);
+        return;
+      }
+
+      // same slot
+      if (sourceContainer === over.id) return;
+
+      setCoursesOnGrid((prev) => ({
+        ...prev,
+        [over.id]: active.id,
+        ...(sourceContainer && { [sourceContainer]: "" }),
+      }));
+
+      const courseAtDestination =
+        coursesOnGrid[over.id as keyof typeof coursesOnGrid];
+
+      // destination is empty
+      if (courseAtDestination === "") {
+        setCoursesUsed({
+          ...coursesUsed,
+          [active.id]: over.id as GridPosition,
+        });
+        return;
+      }
+
+      // destination already has a course
+      setCoursesUsed({
+        ...coursesUsed,
+        [active.id]: over.id as GridPosition,
+        [courseAtDestination]: "",
+      });
+    } else {
+      // return course to the bucket
+      setCoursesOnGrid((prev) => ({
+        ...prev,
+        ...(sourceContainer && { [sourceContainer]: "" }),
+      }));
+      setCoursesUsed({ ...coursesUsed, [active.id]: "" as GridPosition });
+    }
+  };
+
+  const [dropError, setDropError] = useState<DropError>(DropError.NONE);
+  const timeoutRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    if (dropError !== DropError.NONE) {
+      timeoutRef.current = setTimeout(() => {
+        setDropError(DropError.NONE);
+      }, 2000);
+    }
+
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [dropError]);
+
+  const buttonStyle = `
+        w-full px-4 py-1 
+        ring-1 ring-opacity-80
+        rounded-lg shadow-sm 
+        bg-white 
+        hover:text-white hover:bg-opacity-90
+        font-medium
+        transition-all duration-300
+        hover:duration-100 active:scale-95
+    `; // specify default text- color, hover:bg- color, and ring- color
+
+  const clearGrid = () => {
+    setCoursesUsed(() => {
+      const posMap: CoursesUsed = {};
+      Object.keys(courses).forEach((courseCode) => (posMap[courseCode] = ""));
+      return posMap;
+    });
+    setCoursesOnGrid({
+      "3F.1": "",
+      "3F.2": "",
+      "3F.3": "",
+      "3F.4": "",
+      "3F.5": "",
+      "3S.1": "",
+      "3S.2": "",
+      "3S.3": "",
+      "3S.4": "",
+      "3S.5": "",
+      "4F.1": "",
+      "4F.2": "",
+      "4F.3": "",
+      "4F.4": "",
+      "4F.5": "",
+      "4S.1": "",
+      "4S.2": "",
+      "4S.3": "",
+      "4S.4": "",
+      "4S.5": "",
+      "XX.1": "",
+      "XX.2": "",
+      "XX.3": "",
+      "XX.4": "",
+      "XX.5": "",
+    });
+  };
+
+  const screenshotRef = createRef<HTMLDivElement>();
+  const [, takeScreenshot] = useScreenshot({
+    type: "image/png",
+    quality: 1.0,
+  });
+
+  const download = (
+    image: string,
+    { name = "pathmaker", extension = "png" } = {}
+  ) => {
+    const a = document.createElement("a");
+    a.href = image;
+    a.download = createFileName(extension, name);
+    a.click();
+  };
+
+  const downloadScreenshot = () => {
+    takeScreenshot(screenshotRef.current).then(download);
+  };
+
+  return (
+    <DndContext
+      onDragEnd={handleDragEnd}
+      onDragStart={handleDragStart}
+      sensors={sensors}
+    >
+      <section className="flex lg:flex-row flex-col gap-x-6 gap-y-4">
+        <div
+          className="grid grid-cols-5 gap-2 size-max flex-shrink-0"
+          ref={screenshotRef}
+        >
+          {Object.entries(coursesOnGrid).map(([slot, courseCode]) => (
+            <Droppable
+              key={slot}
+              id={slot}
+              valid={validYearTerms[getYearTerm(slot as GridPosition)]}
+            >
+              {courseCode ? (
+                <MakerCard
+                  valid={validYearTerms[getYearTerm(slot as GridPosition)]}
+                  id={courseCode}
+                  code={courseCode}
+                  setCustomInfo={setCustomInfo}
+                  setPreqString={setPreqString}
+                  {...courses[courseCode]}
+                />
+              ) : (
+                slot
+              )}
+            </Droppable>
+          ))}
+        </div>
+        <div>
+          {/* Courses to choose from */}
+          <h2 className="lg:block hidden mb-1 text-xl font-medium text-center">
+            👈 Drag courses into the grid
+          </h2>
+          <h2 className="lg:block hidden text-center mb-1">
+            🔍 Click a course to view more details
+          </h2>
+          <div className="lg:mt-4 flex xl:gap-8 lg:gap-4 gap-8">
+            <button
+              onClick={downloadScreenshot}
+              className={`
+                                ${buttonStyle}
+                                ring-green3
+                                text-green3
+                                hover:bg-green2
+                            `}
+            >
+              Screenshot
+            </button>
+            <button
+              onClick={clearGrid}
+              data-testid="clear-grid"
+              className={`
+                                ${buttonStyle}
+                                ring-comp3
+                                text-comp3
+                                hover:bg-comp2
+                            `}
+            >
+              Clear
+            </button>
+          </div>
+          <h2 className="lg:hidden block my-2 text-lg text-center">
+            ☝️ Hold and drag courses into the grid,
+            <br />
+            🔍 Click a course to view more details
+          </h2>
+          <div
+            className="
+                        flex flex-wrap gap-2 
+                        lg:max-h-[37.5rem] max-h-[22rem]
+                        lg:w-full w-wps 
+                        overflow-y-auto 
+                        justify-center
+                        mt-4
+                    "
+          >
+            {(() => {
+              const filteredCourses = Object.entries(coursesUsed)
+                .filter(([, isUsed]) => !isUsed)
+                .filter(([courseCode]) => filterCourses(courseCode));
+
+              return filteredCourses.length ? (
+                filteredCourses.map(([courseCode]) => (
+                  <MakerCard
+                    valid={true}
+                    key={courseCode}
+                    id={courseCode}
+                    code={courseCode}
+                    setCustomInfo={setCustomInfo}
+                    setPreqString={setPreqString}
+                    {...courses[courseCode]}
+                  />
+                ))
+              ) : (
+                <div
+                  className="
+                                    w-full text-center 
+                                    text-neutral3 italic select-none"
+                >
+                  No courses match the current filter
+                </div>
+              );
+            })()}
+          </div>
+        </div>
+      </section>
+      {createPortal(
+        <DragOverlay>
+          {activeCourse && (
+            <MakerCard
+              valid={true}
+              id={activeCourse as string}
+              code={activeCourse as string}
+              setCustomInfo={setCustomInfo}
+              setPreqString={setPreqString}
+              {...courses[activeCourse]}
+            />
+          )}
+        </DragOverlay>,
+        document.body
+      )}
+      {dropError !== DropError.NONE && (
+        <Announcement>
+          {dropError === DropError.TERM
+            ? "Invalid term for this course!"
+            : "Missing prerequisites!"}
+        </Announcement>
+      )}
+      <Filter filters={filters} setFilters={setFilters} />
+      <WillYouGraduate conditions={conditions} />
+    </DndContext>
+  );
+};
+
+export default CourseGrid;
--- src/pages/Maker.tsx
+++ src/pages/Maker.tsx
@@ -1,125 +1,154 @@
-import CourseGrid from '../components/CourseGrid';
-import CourseForm from '../components/CourseForm';
-import LoadLayout from '../components/LoadLayout';
-import StringDisplay from '../components/StringDisplay';
+import CourseGrid from "../components/grid/CourseGrid";
+import CourseForm from "../components/CourseForm";
+import LoadLayout from "../components/LoadLayout";
+import StringDisplay from "../components/StringDisplay";
 
-import mockCourses from '../data/mockCourses';
-import { useMemo, useState, useEffect } from 'react';
-import { CourseCardProps, CoursesOnGrid, CoursesUsed } from '../types/CourseTypes';
+import mockCourses from "../data/mockCourses";
+import { useMemo, useState, useEffect } from "react";
+import {
+  CourseCardProps,
+  CoursesOnGrid,
+  CoursesUsed,
+} from "../types/CourseTypes";
 
 const Maker = () => {
-    // Course info for custom course
-    const [customInfo, setCustomInfo] = useState<CourseCardProps>({
-        code: "",
-        name: "",
-        preq: [],
-        streams: [],
-        color: "E0E0E0",
-        isCS: false,
-        isHSS: false,
-        isSciMath: false,
-        isArtSci: false,
-        onlyF: false,
-        onlyS: false,
-    });
-    const [preqString, setPreqString] = useState("");
+  // Course info for custom course
+  const [customInfo, setCustomInfo] = useState<CourseCardProps>({
+    code: "",
+    name: "",
+    preq: [],
+    streams: [],
+    color: "E0E0E0",
+    isCS: false,
+    isHSS: false,
+    isSciMath: false,
+    isArtSci: false,
+    onlyF: false,
+    onlyS: false,
+  });
+  const [preqString, setPreqString] = useState("");
 
-    // Load data from localStorage or use default values
-    const [courses, setCourses] = useState(() => {
-        const savedCourses = localStorage.getItem('courses');
-        if (savedCourses) return JSON.parse(savedCourses);
-        return mockCourses;
-    });
+  // Load data from localStorage or use default values
+  const [courses, setCourses] = useState(() => {
+    const savedCourses = localStorage.getItem("courses");
+    if (savedCourses) return JSON.parse(savedCourses);
+    return mockCourses;
+  });
 
-    const initialCoursesUsed = useMemo<CoursesUsed>(() => {
-        const savedCoursesUsed = localStorage.getItem('coursesUsed');
-        if (savedCoursesUsed) return JSON.parse(savedCoursesUsed);
+  const initialCoursesUsed = useMemo<CoursesUsed>(() => {
+    const savedCoursesUsed = localStorage.getItem("coursesUsed");
+    if (savedCoursesUsed) return JSON.parse(savedCoursesUsed);
 
-        const posMap: CoursesUsed = {};
-        Object.keys(courses).forEach(courseCode => posMap[courseCode] = '');
-        return posMap;
-    }, [courses]);
-    
-    const initialCoursesOnGrid = useMemo<CoursesOnGrid>(() => {
-        const savedCoursesOnGrid = localStorage.getItem('coursesOnGrid');
-        if (savedCoursesOnGrid) return JSON.parse(savedCoursesOnGrid);
+    const posMap: CoursesUsed = {};
+    Object.keys(courses).forEach((courseCode) => (posMap[courseCode] = ""));
+    return posMap;
+  }, [courses]);
 
-        return {
-            '3F.1': '', '3F.2': '', '3F.3': '', '3F.4': '', '3F.5': '',
-            '3S.1': '', '3S.2': '', '3S.3': '', '3S.4': '', '3S.5': '',
-            '4F.1': '', '4F.2': '', '4F.3': '', '4F.4': '', '4F.5': '',
-            '4S.1': '', '4S.2': '', '4S.3': '', '4S.4': '', '4S.5': '',
-            'XX.1': '', 'XX.2': '', 'XX.3': '', 'XX.4': '', 'XX.5': '',
-        } as CoursesOnGrid;
-    }, []);
-    
-    const [coursesUsed, setCoursesUsed] = useState<CoursesUsed>(initialCoursesUsed);
-    const [coursesOnGrid, setCoursesOnGrid] = useState<CoursesOnGrid>(initialCoursesOnGrid);
+  const initialCoursesOnGrid = useMemo<CoursesOnGrid>(() => {
+    const savedCoursesOnGrid = localStorage.getItem("coursesOnGrid");
+    if (savedCoursesOnGrid) return JSON.parse(savedCoursesOnGrid);
 
-    // Save courses to localStorage
-    useEffect(() => {
-        localStorage.setItem('courses', JSON.stringify(courses));
-    }, [courses]);
+    return {
+      "3F.1": "",
+      "3F.2": "",
+      "3F.3": "",
+      "3F.4": "",
+      "3F.5": "",
+      "3S.1": "",
+      "3S.2": "",
+      "3S.3": "",
+      "3S.4": "",
+      "3S.5": "",
+      "4F.1": "",
+      "4F.2": "",
+      "4F.3": "",
+      "4F.4": "",
+      "4F.5": "",
+      "4S.1": "",
+      "4S.2": "",
+      "4S.3": "",
+      "4S.4": "",
+      "4S.5": "",
+      "XX.1": "",
+      "XX.2": "",
+      "XX.3": "",
+      "XX.4": "",
+      "XX.5": "",
+    } as CoursesOnGrid;
+  }, []);
 
-    // Save coursesUsed to localStorage
-    useEffect(() => {
-        localStorage.setItem('coursesUsed', JSON.stringify(coursesUsed));
-    }, [coursesUsed]);
+  const [coursesUsed, setCoursesUsed] =
+    useState<CoursesUsed>(initialCoursesUsed);
+  const [coursesOnGrid, setCoursesOnGrid] =
+    useState<CoursesOnGrid>(initialCoursesOnGrid);
 
-    // Save coursesOnGrid to localStorage
-    useEffect(() => {
-        localStorage.setItem('coursesOnGrid', JSON.stringify(coursesOnGrid));
-    }, [coursesOnGrid]);
+  // Save courses to localStorage
+  useEffect(() => {
+    localStorage.setItem("courses", JSON.stringify(courses));
+  }, [courses]);
 
-    // Save everything when component unmounts (e.g. navigate to another page)
-    useEffect(() => {
-        return () => {
-            localStorage.setItem('courses', JSON.stringify(courses));
-            localStorage.setItem('coursesUsed', JSON.stringify(coursesUsed));
-            localStorage.setItem('coursesOnGrid', JSON.stringify(coursesOnGrid));
-        };
-    }, [courses, coursesUsed, coursesOnGrid]);
-    
-    return (
+  // Save coursesUsed to localStorage
+  useEffect(() => {
+    localStorage.setItem("coursesUsed", JSON.stringify(coursesUsed));
+  }, [coursesUsed]);
+
+  // Save coursesOnGrid to localStorage
+  useEffect(() => {
+    localStorage.setItem("coursesOnGrid", JSON.stringify(coursesOnGrid));
+  }, [coursesOnGrid]);
+
+  // Save everything when component unmounts (e.g. navigate to another page)
+  useEffect(() => {
+    return () => {
+      localStorage.setItem("courses", JSON.stringify(courses));
+      localStorage.setItem("coursesUsed", JSON.stringify(coursesUsed));
+      localStorage.setItem("coursesOnGrid", JSON.stringify(coursesOnGrid));
+    };
+  }, [courses, coursesUsed, coursesOnGrid]);
+
+  return (
+    <div>
+      <CourseGrid
+        courses={courses}
+        coursesUsed={coursesUsed}
+        coursesOnGrid={coursesOnGrid}
+        setCoursesUsed={setCoursesUsed}
+        setCoursesOnGrid={setCoursesOnGrid}
+        setCustomInfo={setCustomInfo}
+        setPreqString={setPreqString}
+      />
+      <hr className="mt-8 stroke-2" />
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-x-16">
         <div>
-            <CourseGrid 
-                courses={courses}
-                coursesUsed={coursesUsed} 
-                coursesOnGrid={coursesOnGrid}
-                setCoursesUsed={setCoursesUsed}
-                setCoursesOnGrid={setCoursesOnGrid}
-                setCustomInfo={setCustomInfo}
-                setPreqString={setPreqString}
-            />
-            <hr className="mt-8 stroke-2"/>
-            <div className="grid md:grid-cols-2 grid-cols-1 gap-x-16">
-                <div>
-                    <h2 className="mt-10 mb-6 text-2xl font-semibold">Save/load layout</h2>
-                    <p className="mb-2 max-w-xl">Copy this string and save it for later</p>
-                    <StringDisplay
-                        courses={courses}
-                        coursesOnGrid={coursesOnGrid}
-                    />
-                    <p className="mt-8 mb-2 max-w-xl">Paste your previously copied string below to load it</p>
-                    <LoadLayout 
-                        courses={courses}
-                        coursesUsed={coursesUsed} 
-                        setCourses={setCourses}
-                        setCoursesUsed={setCoursesUsed}
-                        setCoursesOnGrid={setCoursesOnGrid}
-                    />
-                </div>
-                <CourseForm 
-                    setCourses={setCourses}
-                    setCoursesUsed={setCoursesUsed}
-                    customInfo={customInfo}
-                    setCustomInfo={setCustomInfo}
-                    preqString={preqString}
-                    setPreqString={setPreqString}
-                />
-            </div>
+          <h2 className="mt-10 mb-6 text-2xl font-semibold">
+            Save/load layout
+          </h2>
+          <p className="mb-2 max-w-xl">
+            Copy this string and save it for later
+          </p>
+          <StringDisplay courses={courses} coursesOnGrid={coursesOnGrid} />
+          <p className="mt-8 mb-2 max-w-xl">
+            Paste your previously copied string below to load it
+          </p>
+          <LoadLayout
+            courses={courses}
+            coursesUsed={coursesUsed}
+            setCourses={setCourses}
+            setCoursesUsed={setCoursesUsed}
+            setCoursesOnGrid={setCoursesOnGrid}
+          />
         </div>
-    )
-}
+        <CourseForm
+          setCourses={setCourses}
+          setCoursesUsed={setCoursesUsed}
+          customInfo={customInfo}
+          setCustomInfo={setCustomInfo}
+          preqString={preqString}
+          setPreqString={setPreqString}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Maker;
