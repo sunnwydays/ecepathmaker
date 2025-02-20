528823c96a5dffc121cffa9b946532b7ac93a0dd
sunny <sunnwydays@gmail.com>
2025-02-20 10:52:22 -0500

    Create pageImports file

---
+++ src/utils/pageImports.ts
@@ -0,0 +1,4 @@
+export { default as Courses } from "../pages/Courses";
+export { default as Maker } from "../pages/Maker";
+export { default as FAQ } from "../pages/FAQ";
+export { default as NotFound } from "../pages/NotFound";
