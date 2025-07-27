66f8a05c3ad28b3f82698e020fa168f1fed8a988
Sunny <sunnwydays@gmail.com>
2025-07-27 17:28:38 -0400

    Assume colour does not have '#' in getTextColor

--- src/utils/getTextColor.tsx
+++ src/utils/getTextColor.tsx
@@ -1,7 +1,7 @@
 export const getTextColor = (color: string | undefined): string => {
-    const hexColor = color ? `#${color}` : 'E0E0E0';
+    const hexColor = color ? `${color}` : 'E0E0E0';
     
-    const rgb = parseInt(hexColor.slice(1), 16); // Convert hex to RGB
+    const rgb = parseInt(hexColor, 16); // Convert hex to RGB
     const r = (rgb >> 16) & 0xff;
     const g = (rgb >>  8) & 0xff;
     const b = (rgb >>  0) & 0xff;
